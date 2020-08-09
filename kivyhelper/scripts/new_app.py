e079b855c90ac112a22aa7348ebe8c1a67135e99
Larquebus <quill9@gmail.com>
2020-08-08 21:34:52 -0500

    Built out the basic functionality of new_app and added setup_kivy_app_py.

--- kivyhelper/scripts/new_app.py
+++ kivyhelper/scripts/new_app.py
@@ -1,8 +1,14 @@
 import argparse
-import os
+from pathlib import Path
 
 
 def assemble_args():
+    """
+    Collects the necessary args for the new_app script.
+
+    Returns: The collected args Namespace.
+
+    """
     parser = argparse.ArgumentParser(
         "Populate a directory with the basics needed for a Kivy app."
     )
@@ -26,12 +32,42 @@ def assemble_args():
     return parser.parse_args()
 
 
+def setup_kivy_app_py(app_name: str) -> str:
+    """
+    Generates a file string for the *App python file in the root kivy
+    app.
+
+    Args:
+        app_name: A string, the name of the app.
+
+    Returns: A string, the python code to save to the App.py file.
+
+    """
+    return (
+        "from kivy.app import App\n"
+        "from kivy.uix.floatlayout import FloatLayout\n\n\n"
+        "class AppFrame(FloatLayout):\n"
+        "    pass\n\n\n"
+        f"class {app_name}App(App):\n"
+        "    def build(self):\n"
+        "        return AppFrame()\n\n\n"
+        "if __name__ == '__main__':\n"
+        f"    {app_name}App().run()\n"
+    )
+
+
 def execute():
     args = assemble_args()
+    a = args.app_name
+    d = Path(args.dir) if args.dir else Path.cwd()
+    print(f'[KIVYHELPER:new_app] Creating Kivy app {a} in {d}...')
 
-    d = args.dir if args.dir else os.getcwd()
+    app_dir = d.joinpath(a)
+    app_dir.mkdir(exist_ok=True)
 
-    print(f'[KIVYHELPER:new_app] Creating Kivy app {args.app_name} in {d}...')
+    print(f'-- Creating {a}App.py in {app_dir}...')
+    app_py = app_dir.joinpath(f'{a}App.py')
+    app_py.write_text(setup_kivy_app_py(a))
 
 
 if __name__ == "__main__":
