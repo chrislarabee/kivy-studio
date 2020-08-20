677190b0300b5c05e56282782cd1445dec3d2ee0
Larquebus <quill9@gmail.com>
2020-08-19 19:58:28 -0500

    Yet more setup tweaks.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     url='https://github.com/Larquebus/kivyhelper',
     author='Chris Larabee',
     author_email='quill9@gmail.com',
-    packages=setuptools.find_packages(),
+    packages=['kivyhelper', 'kivyhelper.scripts', 'kivyhelper.widgets],
     install_requires=[],
     version='0.1.0',
     license='Creative Commons',
