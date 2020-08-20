a56e93896371d3f26a148cf53f3d31a890ac0daa
Larquebus <quill9@gmail.com>
2020-08-19 20:17:12 -0500

    Removed dependency links as that functionality is deprecated for git.

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setuptools.setup(
         'kivy_deps.glew==0.1.*',
         'kivy_deps.gstreamer==0.1.*',
         'kivy==1.11.1',
-    ],
-    dependency_links=[
-        'https://github.com/Larquebus/Amanuensis#egg=amanuensis',
+        'amanuensis @ git+https://github.com/Larquebus/Amanuensis#egg=amanuensis'
     ],
     version='0.1.0',
     license='Creative Commons',
