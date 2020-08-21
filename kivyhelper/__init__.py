6430597b0d454def9e4457c32c708243d9038b6a
Larquebus <quill9@gmail.com>
2020-08-21 14:10:27 -0500

    Experiment for import troubleshooting.

--- kivyhelper/__init__.py
+++ kivyhelper/__init__.py
@@ -1,6 +1,6 @@
 import os
 os.environ['KIVY_NO_ARGS'] = '1'
-os.environ['KIVY_NO_CONSOLELOG'] = '1'
+# os.environ['KIVY_NO_CONSOLELOG'] = '1'
 from . import scripts, widgets
 from .codex import Codex, Node
 
