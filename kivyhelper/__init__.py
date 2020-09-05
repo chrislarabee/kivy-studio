e2d6d27510473afbe50cb8b68593d9298bf690ae
Larquebus <quill9@gmail.com>
2020-09-05 14:14:35 -0500

    v0.2.2: Another patch for import structure.

--- kivyhelper/__init__.py
+++ kivyhelper/__init__.py
@@ -1,8 +1,13 @@
 import os
 os.environ['KIVY_NO_ARGS'] = '1'
 
-from .scripts import build_assets, new_app
-from . import widgets
+from . import scripts
 from .codex import Codex, Node
+from .scripts.build_assets.lib import build_assets_folder
+from .scripts.new_app.lib import create_new_app
+from .widgets import AnimRule, Sprite
 
-__all__ = ['scripts', 'widgets', 'Codex', 'Node']
+__all__ = [
+    'scripts', 'Codex', 'Node', 'Sprite', 'AnimRule', 'build_assets_folder',
+    'create_new_app',
+]
