1c1130b8e864cdaf75d0bc3821f74fb40f95c330
Larquebus <quill9@gmail.com>
2020-09-05 16:50:21 -0500

    v0.2.3: Further import structure tweaks.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         'amanuensis @ git+https://github.com/Larquebus/'
         'Amanuensis#egg=amanuensis'
     ],
-    version='0.2.2',
+    version='0.2.3',
     license='Creative Commons',
     description=('A suite of pre-built widgets and helper scripts to '
                  'facilitate building a Kivy-based app. '),
