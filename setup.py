e7e1c3e9effbea321c17c62189e4c45c74aad521
Larquebus <quill9@gmail.com>
2020-09-05 14:14:51 -0500

    v0.2.2: Updated setup.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         'amanuensis @ git+https://github.com/Larquebus/'
         'Amanuensis#egg=amanuensis'
     ],
-    version='0.2.1',
+    version='0.2.2',
     license='Creative Commons',
     description=('A suite of pre-built widgets and helper scripts to '
                  'facilitate building a Kivy-based app. '),
