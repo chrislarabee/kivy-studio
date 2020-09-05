c7d33b2faffecbc2195af516ad894796720d2895
Larquebus <quill9@gmail.com>
2020-09-05 16:52:28 -0500

    v0.2.4: Added scripts packages to setup.

--- kivyhelper/__init__.py
+++ kivyhelper/__init__.py
@@ -1,7 +1,7 @@
 import os
 os.environ['KIVY_NO_ARGS'] = '1'
 
-from .scripts import build_assets, new_app
+from . import scripts
 from .codex import Codex, Node
 from .scripts.build_assets.lib import build_assets_folder
 from .scripts.new_app.lib import create_new_app
--- setup.py
+++ setup.py
@@ -5,7 +5,13 @@ setuptools.setup(
     url='https://github.com/Larquebus/kivyhelper',
     author='Chris Larabee',
     author_email='quill9@gmail.com',
-    packages=['kivyhelper', 'kivyhelper.scripts', 'kivyhelper.widgets'],
+    packages=[
+        'kivyhelper',
+        'kivyhelper.scripts',
+        'kivyhelper.widgets',
+        'kivyhelper.scripts.build_assets',
+        'kivyhelper.scripts.new_app',
+    ],
     install_requires=[
         'docutils',
         'pygments',
@@ -18,7 +24,7 @@ setuptools.setup(
         'amanuensis @ git+https://github.com/Larquebus/'
         'Amanuensis#egg=amanuensis'
     ],
-    version='0.2.3',
+    version='0.2.4',
     license='Creative Commons',
     description=('A suite of pre-built widgets and helper scripts to '
                  'facilitate building a Kivy-based app. '),
