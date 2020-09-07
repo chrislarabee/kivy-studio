51cfde959df00cb3378521b4b70981e20ad45170
Larquebus <quill9@gmail.com>
2020-09-06 20:12:14 -0500

    v0.3.2: Last commit was ill-advised.

--- kivyhelper/__init__.py
+++ kivyhelper/__init__.py
@@ -7,10 +7,8 @@ from .scripts.build_assets.lib import build_assets_folder
 from .scripts.new_app.lib import create_new_app
 from .widgets import (AnimRule, Sprite, DialogueBox, DialogueLines,
                       DialogueLine)
-from amanuensis.util import cascade
 
 __all__ = [
     'scripts', 'Codex', 'Node', 'Sprite', 'AnimRule', 'build_assets_folder',
     'create_new_app', 'DialogueBox', 'DialogueLines', 'DialogueLine',
-    'cascade',
 ]
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
         'amanuensis @ git+https://github.com/Larquebus/'
         'Amanuensis#egg=amanuensis'
     ],
-    version='0.3.1',
+    version='0.3.2',
     license='Creative Commons',
     description=('A suite of pre-built widgets and helper scripts to '
                  'facilitate building a Kivy-based app. '),
